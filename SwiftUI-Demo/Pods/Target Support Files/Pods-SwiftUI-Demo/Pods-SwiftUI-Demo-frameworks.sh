9c1e8101edd54a2e7db80aa6ddb02ffbaee91efc
zhang <1124700416@qq.com>
2021-03-05 12:52:53 +0800

    swiftUI-

--- SwiftUI-Demo/Pods/Target Support Files/Pods-SwiftUI-Demo/Pods-SwiftUI-Demo-frameworks.sh
+++ SwiftUI-Demo/Pods/Target Support Files/Pods-SwiftUI-Demo/Pods-SwiftUI-Demo-frameworks.sh
@@ -176,9 +176,11 @@ code_sign_if_enabled() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/AEAlertView/AEAlertView.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/AEAlertView/AEAlertView.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
--- SwiftUI-Demo/SwiftUI-Demo/Classes/UI/IndexView.swift
+++ SwiftUI-Demo/SwiftUI-Demo/Classes/UI/IndexView.swift
@@ -7,10 +7,16 @@
 
 import SwiftUI
 import AEAlertView
+import SnapKit
 
 
 struct AEIndexView: View {
     
+    
+    @State private var showSheet: Bool = false
+
+    @State private var subviewOffset: CGFloat = 50
+
     var body: some View {
         ZStack {
             Rectangle().foregroundColor(.white)
@@ -20,10 +26,72 @@ struct AEIndexView: View {
                         print("1111")
                     }
                 }
+                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: .top)
+                .background(Color.red)
+                .shadow(radius: 25)
+                .padding(.bottom, 4)
+                .hidden()
                 
+                
+                Text("管理员").font(Font.system(size: 8))
+                    if showSheet {
+                        .hidden()
+                    }
+                
+                
+                Button("sheet") {
+//                    self.alertBtn.frame(width: 100, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: .top)
+//                    self.alertBtn.padding(.leading)
+
+                    self.subviewOffset = 100
+                    self.showSheet = true
+
+                }
+                HStack {
+                    subView.padding(.leading, subviewOffset)
+                    Spacer()
+                    alertBtn
+                }.padding()
+                    
+
+
+                .actionSheet(isPresented: $showSheet, content: {sheet})
             }
         }
     }
+
+    let alertBtn = Button("11145676786") {
+
+    }
+
+    var subView: SubView {
+        let v = SubView()
+
+        return v
+    }
+
+    private var alert: AEAlertView {
+        let view = AEAlertView(style: .defaulted, title: "title", message: "message")
+
+        return view
+    }
+
+    private var sheet: ActionSheet {
+        let v = ActionSheet(title: Text("title"), message: Text("msg"), buttons: [.destructive(Text("def"), action: {
+            print("Default")
+            self.showSheet = false
+        })])
+
+        return v
+    }
     
 }
+
+struct SubView: View {
+    var body: some View {
+        Text("subView")
+            
+    }
+}
+
 
